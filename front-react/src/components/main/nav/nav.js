f9b357862b9372b03f0e468d6c8f4e6452a83b08
doyou1 <tla149@daum.net>
2023-08-02 23:37:50 +0900

    for sinhj

--- front-react/src/components/main/nav/nav.js
+++ front-react/src/components/main/nav/nav.js
@@ -1,9 +1,21 @@
+import logo from "images/home-logo.svg";
+
 export default function Nav() {
     return (
-        <nav>
+        <nav className="flex">
             {/* home logo */}
+            <div className="flex items-center">
+                <a className="" href="#">
+                    <img className="w-10 h-10" src={logo} alt="home logo"/>
+                </a>
+            </div>
 
             {/* search bar */}
+            <div className="flex justify-center items-center w-full">
+                <button type="button" className="flex relative items-center w-full rounded-full bg-gray-30">
+                    Search
+                </button>
+            </div>
 
             {/* buttons */}
                 {/* Learn */}
