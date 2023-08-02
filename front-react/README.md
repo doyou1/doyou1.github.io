b0fa98d39c80a3ace92cbc728538a98af57dcf5e
신현진 <blackduvet52@gmail.com>
2023-08-03 02:59:11 +0900

    [현진] intro layout 구성완료

--- front-react/README.md
+++ front-react/README.md
@@ -1,12 +1,7 @@
 # React 공식 사이트 클론
+
 - 메인 페이지 분석(추후 이미지 수정 예정)
-![alt main](./screenshots/main-without-footer.png)
-![alt main-footer](./screenshots/main-footer.png)
-    1. Nav
-    2. Intro
-    3. Summary
-    4. SummaryButton
-    5. SummaryButtonNews
-    6. SummaryButtonSlider
-    7. Footer
-    > 총 7개의 컴포넌트로 나눌 수 있다고 분석됨(팀원과 회의 필요)
+  ![alt main](./screenshots/main-without-footer.png)
+  ![alt main-footer](./screenshots/main-footer.png) 1. Nav 2. Intro 3. Summary 4. SummaryButton 5. SummaryButtonNews 6. SummaryButtonSlider 7. Footer > 총 7개의 컴포넌트로 나눌 수 있다고 분석됨(팀원과 회의 필요)
+
+### 서로 버전이 달라서 신현진의 경우는 npm start 가 아니라 $ npm run start로 실행시켜야한다
--- front-react/src/components/main/items/intro.js
+++ front-react/src/components/main/items/intro.js
@@ -1,9 +1,32 @@
-import 'styles/intro.css';
+import "styles/intro.css";
+import logo from "images/home-logo.svg";
 
 export default function Intro() {
-    return (
-        <>
-            <div>Intro</div>
-        </>
-    );
+  return (
+    <>
+      {/* {main intro} */}
+      <div className="intro-container">
+        {/* home logo */}
+        <div className="flex items-center">
+          <a className="" href="#">
+            <img className="react-logo " src={logo} alt="home logo" />
+          </a>
+        </div>
+        {/* text set */}
+        <h1 className="text-h1 font-diplay text-xl">React</h1>
+        <p className="text-p font-diplay">
+          The library for web and native user interfaces
+        </p>
+        {/* button set */}
+        <div className="button-boxes">
+          <a href="#" className="learn-react-box" aria-label=" Learn React">
+            Learn React
+          </a>
+          <a href="#" className="api-reference-box" aria-label="API Reference">
+            API Reference
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
