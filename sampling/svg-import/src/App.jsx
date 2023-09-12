13c92f1b9884f2653fdb50a3ad9614bf1f6600d1
doyou1 <tla149@daum.net>
2023-09-12 22:11:33 +0900

    svg component

--- sampling/svg-import/src/App.jsx
+++ sampling/svg-import/src/App.jsx
@@ -1,53 +1,12 @@
-import SpriteIcon from "@/assets/icons/sprite.svg";
-import ReactIcon from "@/assets/icons/react.svg";
-
-import { useState } from "react";
-import { HuePicker } from 'react-color';
+import {ReactComponent as ReactIcon} from "@/assets/icons/react.svg";
 import "@/App.css";
 
 function App() {
-  const titles = ["<img>로 SVG 로딩하기", "SVG 스프라이트 - use를 사용하기", "다크모드"];
-
-  const [spriteSize, setSpriteSize] = useState(32);
-  const [spriteColor, setSpriteColor] = useState("#00D8FF");
-  const [isDark, setIsDark] = useState(false);
-  
   return (
     <>
-      {/* <img>로 SVG 로딩하기 */}
-      <h1>{ titles[0] }</h1>
-      <img src={ReactIcon} loading="lazy" />
-
-      {/* SVG 스프라이트 - use를 사용하기 */}
-      <h1>{ titles[1] }</h1>
-        {/* Edit Form */}
-        <div>
-          Size : <input type="number" value={spriteSize} onChange={(e) => setSpriteSize(Number(e.target.value))} />
-        </div>
-        <div>
-          Color Picker(current: <span style={{color: spriteColor}}>{ spriteColor }</span>) : 
-            <HuePicker 
-              color={spriteColor}
-              onChangeComplete={(color) => setSpriteColor(color.hex) }
-            />
-        </div>
-      {/* !!! 중요 !!! */}
-      <div>
-        <svg width={spriteSize} height={spriteSize}  >
-          <use href={`${SpriteIcon}#react`} width={spriteSize} height={spriteSize} style={{color: spriteColor}}/>
-        </svg>
-      </div>
-      
-      <h1>{ titles[2] }</h1>
-      <div>
-        <button type="button" onClick={() => setIsDark(!isDark)}>다크모드 on/off</button> status: { isDark ? "다크모드 ON" : "다크모드 OFF" }
-      </div>
-      <div>
-        <svg className={"w-32 h-32 " + (isDark ? "dark" : "")}>
-          <use href={`${SpriteIcon}#react`} className="w-32 h-32" />
-        </svg>
-      </div>
-      
+      <ReactIcon style={{ color: 'red' }}/>
+      <ReactIcon style={{ width: '100px', height: '100px', color: 'blue' }}/>
+      <ReactIcon style={{ backgroundColor: 'black', color: 'white'}}/>
     </>
   )
 }
--- sampling/svg-import/vite.config.js
+++ sampling/svg-import/vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  modules: {
-    rules: [
-      {
-        test: /\.svg/,
-        type: "assets/icons",
-      }
-    ],
-  },
   resolve: {
     alias: {
       '@/': `${__dirname}/src/`,
     },
   },
-  plugins: [react()],
+  plugins: [svgr(), react()],
 })
