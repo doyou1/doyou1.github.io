606f87b26a831f26ae89725bdfcdd846e4d58005
doyou1 <tla149@daum.net>
2023-09-30 16:22:35 +0900

    docSearch - 검색, 검색 로딩중 관련 알고리즘 추가 - 검색 결과에 따른 Dropdown 페이지 연동 로직(최초, 검색 결과, 검색 실패시) - footer 디자인 완료(svg 외부 파일화 등) - (!필요!) json 서버 추가해서 api 연동 로직 (callApi 함수 수정 필요함)

--- front-react/src/components/main/nav/docSearch.js
+++ front-react/src/components/main/nav/docSearch.js
@@ -5,60 +5,67 @@ import { useSetRecoilState } from "recoil";
 import { ReactComponent as SearchIcon } from "@/assets/nav/search.svg";
 import { ReactComponent as LoadingIcon } from "@/assets/nav/loading.svg";
 import { ReactComponent as CloseIcon } from "@/assets/nav/close.svg";
+import { ReactComponent as HomeMenuIcon } from "@/assets/nav/home-menu.svg";
+import { ReactComponent as ActionIcon } from "@/assets/nav/action.svg";
+import { ReactComponent as AlgoliaIcon } from "@/assets/nav/algolia.svg";
+import { ReactComponent as CommandSelectIcon } from "@/assets/nav/command-select.svg";
+import { ReactComponent as CommandNavigateIcon } from "@/assets/nav/command-navigate.svg";
+import { ReactComponent as CommandCloseIcon } from "@/assets/nav/command-close.svg";
+import { ReactComponent as NoSearchIcon } from "@/assets/nav/no-search.svg";
 
 import { isOpenDocSearchState } from "@/recoil/recoil_state";
-
 
 export default function DocSearch() {
   const [inputValue, setInputValue] = useState("");
   const [isSearching, setIsSearching] = useState(false);
   const [timer, setTimer] = useState(0);
+  const [list, setList] = useState([]);
 
   const setIsOpenDocSearch = useSetRecoilState(isOpenDocSearchState);
 
   const close = () => {
     setIsOpenDocSearch(false);
-  }
+  };
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
 
     debounce(e.target.value);
-  }
+  };
 
   // 디바운싱 - 마지막 호출만 적용
   const debounce = (value) => {
     // 이전 타이머 종료
     clearTimeout(timer);
-    
+
     // 유효성검사 : 입력값 없을시 타이머 설정X
-    if(value.length == 0){
+    if (value.length == 0) {
       // 에러처리 : 입력 중 입력값이 ''이 될 경우에도 로딩아이콘이 돌아가는 에러 해결
-      if(isSearching) setIsSearching(false);
+      if (isSearching) setIsSearching(false);
       return;
     }
 
     // 입력 중: 로딩아이콘 표시O
-    if(!isSearching) setIsSearching(true);
-    
+    if (!isSearching) setIsSearching(true);
+
     // 최신 타이머 설정
     const newTimer = setTimeout(() => {
-      // 
+      // 실제 api 처리 필요
       callApi(value);
 
       // 검색완료: 로딩아이콘 표시X
       setIsSearching(false);
     }, 1000);
     setTimer(newTimer);
-  }
+  };
 
   const callApi = (value) => {
-    console.log('call search api: ', value);
-  }
+    console.log("call search api: ", value);
+  };
 
   const handleReset = () => {
     setInputValue("");
-  }
+  };
 
   return (
     <>
@@ -69,50 +76,302 @@ export default function DocSearch() {
         aria-labelledby="docsearch-label"
         className="DocSearch-Container"
         onClick={close}
+        aria-owns={list.length != 0 ? "docsearch-list" : ""}
       >
-        <div className="DocSearch-Modal bg-white dark:bg-[#23272f]" onClick={(e) => e.stopPropagation()}>
+        <div
+          className="DocSearch-Modal bg-white dark:bg-[#23272f]"
+          onClick={(e) => e.stopPropagation()}
+        >
           <header className="DocSearch-SearchBar">
             <form className="DocSearch-Form" onSubmit={() => false}>
-              <label 
-                className={`DocSearch-MagnifierLabel ${isSearching ? "hidden" : "flex"}`}
-                >
-                  <SearchIcon />
+              <label
+                className={`DocSearch-MagnifierLabel ${
+                  isSearching ? "hidden" : "flex"
+                }`}
+              >
+                <SearchIcon />
               </label>
-              <div 
-                className={`DocSearch-LoadingIndicator ${!isSearching ? "hidden" : "flex"}`}
-                >
-                  <LoadingIcon />
+              <div
+                className={`DocSearch-LoadingIndicator ${
+                  !isSearching ? "hidden" : "flex"
+                }`}
+              >
+                <LoadingIcon />
               </div>
-              <input 
-                className="DocSearch-Input" 
-                aria-autocomplete="both" 
-                aria-labelledby="docsearch-label" 
-                autoComplete="off" 
-                autoCorrect="off" 
-                autoCapitalize="off" 
-                enterKeyHint="search" 
-                spellCheck="false" 
-                placeholder="Search docs" 
-                maxLength="64" 
+              <input
+                className="DocSearch-Input"
+                aria-autocomplete="both"
+                aria-labelledby="docsearch-label"
+                autoComplete="off"
+                autoCorrect="off"
+                autoCapitalize="off"
+                enterKeyHint="search"
+                spellCheck="false"
+                placeholder="Search docs"
+                maxLength="64"
                 type="search"
                 onChange={handleChange}
-                />
-            <button 
-              className={`DocSearch-Reset ${inputValue.length == 0 && "hidden"}`} 
-              type="reset" 
-              title="Clear the query" 
-              onClick={handleReset}
+              />
+              <button
+                className={`DocSearch-Reset ${
+                  inputValue.length == 0 && "hidden"
+                }`}
+                type="reset"
+                title="Clear the query"
+                onClick={handleReset}
               >
                 <CloseIcon />
-            </button>
-          </form>
-            <button 
-              className="DocSearch-Cancel"
-              onClick={close}
-              >
-                Cancel
+              </button>
+            </form>
+            <button className="DocSearch-Cancel" onClick={close}>
+              Cancel
             </button>
           </header>
+          <div className="DocSearch-Dropdown">
+            {list.length == 0 && inputValue.length == 0 && (
+              <div className="DocSearch-StartScreen">
+                <p className="DocSearch-Help">No recent searches</p>
+              </div>
+            )}
+            {list.length == 0 && inputValue.length > 0 && !isSearching && (
+              <div className="DocSearch-NoResults">
+                <div className="DocSearch-Screen-Icon">
+                  <NoSearchIcon/>
+                </div>
+                <p className="DocSearch-Title">
+                  No results for &quot;<strong>{ inputValue }</strong>&quot;
+                </p>
+                <div className="DocSearch-NoResults-Prefill-List">
+                  <p className="DocSearch-Help">Try searching for:</p>
+                  <ul>
+                    <li>
+                      <button className="DocSearch-Prefill">React APIs</button>
+                    </li>
+                  </ul>
+                </div>
+                <p className="DocSearch-Help">
+                  Believe this query should return results?{" "}
+                  <a
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Let us know
+                  </a>
+                  .
+                </p>
+              </div>
+            )}
+            {list.length > 0 && (
+              <div className="DocSearch-Dropdown-Container">
+              <section className="DocSearch-Hits">
+                <div className="DocSearch-Hit-source">React APIs</div>
+                <ul
+                  role="listbox"
+                  aria-labelledby="docsearch-label"
+                  id="docsearch-list"
+                >
+                  <li
+                    className="DocSearch-Hit"
+                    id="docsearch-item-0"
+                    role="option"
+                    aria-selected="false"
+                  >
+                    <a href="#">
+                      <div className="DocSearch-Hit-Container">
+                        <div className="DocSearch-Hit-icon">
+                          <HomeMenuIcon />
+                        </div>
+                        <div className="DocSearch-Hit-content-wrapper">
+                          <span className="DocSearch-Hit-title">
+                            <mark>width</mark>: A string. Specifies the image{" "}
+                            <mark>width</mark> for a type …
+                          </span>
+                          <span className="DocSearch-Hit-path">
+                            &lt;input&gt;
+                          </span>
+                        </div>
+                        <div className="DocSearch-Hit-action">
+                          <svg width="20" height="20" viewBox="0 0 20 20">
+                            <g
+                              stroke="currentColor"
+                              fill="none"
+                              fillRule="evenodd"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            >
+                              <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                              <path d="M8 17l-6-6 6-6"></path>
+                            </g>
+                          </svg>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                  <li
+                    className="DocSearch-Hit"
+                    id="docsearch-item-1"
+                    role="option"
+                    aria-selected="false"
+                  >
+                    <a href="#">
+                      <div className="DocSearch-Hit-Container">
+                        <div className="DocSearch-Hit-icon">
+                          <HomeMenuIcon />
+                        </div>
+                        <div className="DocSearch-Hit-content-wrapper">
+                          <span className="DocSearch-Hit-title">
+                            <mark>width</mark>
+                          </span>
+                          <span className="DocSearch-Hit-path">
+                            Common components (e.g. &lt;div&gt;)
+                          </span>
+                        </div>
+                        <div className="DocSearch-Hit-action">
+                          <ActionIcon />
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                  <li
+                    className="DocSearch-Hit"
+                    id="docsearch-item-2"
+                    role="option"
+                    aria-selected="false"
+                  >
+                    <a href="#">
+                      <div className="DocSearch-Hit-Container">
+                        <div className="DocSearch-Hit-icon">
+                          <HomeMenuIcon />
+                        </div>
+                        <div className="DocSearch-Hit-content-wrapper">
+                          <span className="DocSearch-Hit-title">
+                            cols: A number. Specifies the default{" "}
+                            <mark>width</mark> in average character …
+                          </span>
+                          <span className="DocSearch-Hit-path">
+                            &lt;textarea&gt;
+                          </span>
+                        </div>
+                        <div className="DocSearch-Hit-action">
+                          <ActionIcon />
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+              <section className="DocSearch-Hits">
+                <div className="DocSearch-Hit-source">Learn</div>
+                <ul
+                  role="listbox"
+                  aria-labelledby="docsearch-label"
+                  id="docsearch-list"
+                >
+                  <li
+                    className="DocSearch-Hit"
+                    id="docsearch-item-3"
+                    role="option"
+                    aria-selected="false"
+                  >
+                    <a href="#">
+                      <div className="DocSearch-Hit-Container">
+                        <div className="DocSearch-Hit-icon">
+                          <HomeMenuIcon />
+                        </div>
+                        <div className="DocSearch-Hit-content-wrapper">
+                          <span className="DocSearch-Hit-title">
+                            … example, className, src, alt, <mark>width</mark>,
+                            and height are some …
+                          </span>
+                          <span className="DocSearch-Hit-path">
+                            Passing Props to a Component
+                          </span>
+                        </div>
+                        <div className="DocSearch-Hit-action">
+                          <ActionIcon />
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                  <li
+                    className="DocSearch-Hit"
+                    id="docsearch-item-4"
+                    role="option"
+                    aria-selected="false"
+                  >
+                    <a href="#">
+                      <div className="DocSearch-Hit-Container">
+                        <div className="DocSearch-Hit-icon">
+                          <HomeMenuIcon />
+                        </div>
+                        <div className="DocSearch-Hit-content-wrapper">
+                          <span className="DocSearch-Hit-title">
+                            … For example, instead of stroke-
+                            <mark>width</mark> you use stroke
+                            <mark>Width</mark> …
+                          </span>
+                          <span className="DocSearch-Hit-path">
+                            Writing Markup with JSX
+                          </span>
+                        </div>
+                        <div className="DocSearch-Hit-action">
+                          <ActionIcon />
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+              <section className="DocSearch-HitsFooter"></section>
+            </div>
+            )}
+          </div>
+          <footer className="DocSearch-Footer">
+            <div className="DocSearch-Logo">
+              <a
+                href="https://www.algolia.com/docsearch"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="DocSearch-Label">Search by</span>
+                <AlgoliaIcon />
+              </a>
+            </div>
+            <ul className="DocSearch-Commands">
+              <li>
+                <span className="DocSearch-Commands-Key">
+                  <CommandSelectIcon />
+                </span>
+                <span className="DocSearch-Label">to select</span>
+              </li>
+              <li>
+                <span className="DocSearch-Commands-Key">
+                  <svg width="15" height="15">
+                    <g
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.2"
+                    >
+                      <path d="M7.5 3.5v8M10.5 8.5l-3 3-3-3"></path>
+                    </g>
+                  </svg>
+                </span>
+                <span className="DocSearch-Commands-Key">
+                  <CommandNavigateIcon />
+                </span>
+                <span className="DocSearch-Label">to navigate</span>
+              </li>
+              <li>
+                <span className="DocSearch-Commands-Key">
+                  <CommandCloseIcon />
+                </span>
+                <span className="DocSearch-Label">to close</span>
+              </li>
+            </ul>
+          </footer>
         </div>
       </div>
     </>
