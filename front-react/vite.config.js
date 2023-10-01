0beb81f97e97bd31cea6e8a7922e23752ea46c9a
doyou1 <tla149@daum.net>
2023-10-01 15:18:24 +0900

    vite config 수정

--- front-react/vite.config.js
+++ front-react/vite.config.js
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // github pages 배포용
   base: '/web-study-clone/react',
   resolve: {
     alias: {
--- front-vue/vite.config.js
+++ front-vue/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // github pages 배포용
   base: '/web-study-clone/vue',
   plugins: [
     vue(),
