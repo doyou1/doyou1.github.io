1d71ed43358f56ca5f8b0e4a970b98d6d77033fe
Hyun jin <blackduvet52@gmail.com>
2023-12-08 22:13:18 +0900

    #67 : INTRO TITLE 섹션 추가 및 스타일 적용
    front-vue-ts/src/components/main/items/IntroView.vue
    
    - 'intro-container' 섹션 추가
    - 타이틀과 버튼을 포함한 소개 섹션 작성
    - 섹션에 대한 스타일 및 미디어 쿼리 구현

--- front-vue-ts/src/components/main/items/IntroView.vue
+++ front-vue-ts/src/components/main/items/IntroView.vue
@@ -1,7 +1,66 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div>IntroView</div>
+  <section class="intro-container">
+    <!-- INTRO TITLE  -->
+    <h1 class="intro-container__title-h1">
+      The <span class="intro-container__title-h1__title-span">Progressive</span>
+      <br />
+      JavaScript Framework
+    </h1>
+
+    <!-- INTRO explanation-->
+    <p class="intro-container__explam">
+      An approachable, performant and versatile framework for building web user
+      interfaces.
+    </p>
+
+    <!-- INTRO BUTTON -->
+    <p class="intro-container__button">
+      <a href="#" class="intro-container__button__why-vue">Why vue</a>
+      <a href="#" class="intro-container__button__get-started">Get Started</a>
+      <a href="#" class="intro-container__button__install">Install</a>
+    </p>
+  </section>
 </template>
+
+<!-- Style -->
+<style scoped lang="scss">
+.intro-container {
+  padding: 96px 32px;
+  text-align: center;
+
+  &__title-h1 {
+    color: #213547;
+    font-size: 76px;
+    line-height: 1.25;
+    font-weight: 900;
+    letter-spacing: -1.5px;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+}
+
+// ===============================
+//          MEDIA QUREIES
+// ===============================
+
+/* 미디어 쿼리: 화면 너비가 960px 이하일 때 */
+@media (max-width: 960px) {
+  .intro-container {
+    &__title-h1 {
+      font-size: 64px;
+      letter-spacing: -0.5px;
+    }
+  }
+}
+/* 미디어 쿼리: 화면 너비가 768px 이하일 때 */
+@media (max-width: 768px) {
+  .intro-container {
+    &__title-h1 {
+      font-size: 48px;
+      letter-spacing: -0.5px;
+    }
+  }
+}
+</style>
