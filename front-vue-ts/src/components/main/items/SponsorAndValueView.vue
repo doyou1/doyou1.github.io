00d219e6cbaf3622b295729b3509f4fd06fd5f61
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-12-20 15:00:22 +0900

    feat SponsorAndValueView.vue sponsor카드 이미지 불러오기 수정
    svg가 불러오지 않는 문제 수정

--- front-vue-ts/src/components/main/items/SponsorAndValueView.vue
+++ front-vue-ts/src/components/main/items/SponsorAndValueView.vue
@@ -2,51 +2,212 @@
 <!-- script에 위치한 함수들은 추후 composable로 옮길 예정 -->
 <script setup lang="ts">
 import { ref } from "vue";
-import type { SponsorImage, ImageModules } from "@/composables/use-items";
+import type { SponsorItemListType } from "@/composables/use-items";
 
-const goldImportImages = import.meta.glob(
-  "../../../assets/sponsorList/gold/*.{png,jpeg,jpg,svg,avif}",
-  { eager: true },
-) as ImageModules;
-const platinumImportImages = import.meta.glob(
-  "../../../assets/sponsorList/platinum/*.{png,jpeg,jpg,svg,avif}",
-  { eager: true },
-) as ImageModules;
-
-const goldImage = ref<SponsorImage>([]);
-const platinumImage = ref<SponsorImage>([]);
-
-for (const img in goldImportImages) {
-  goldImage.value.push(goldImportImages[img].default);
-}
-for (const img in platinumImportImages) {
-  platinumImage.value.push(platinumImportImages[img].default);
-}
-console.log("goldImage : ", goldImage.value);
+const imageList = ref<SponsorItemListType>([
+  {
+    sponsorGrade: "Platinum",
+    sponsorDetail: [
+      {
+        sponsorUrl: "https://bit.dev",
+        imgUrl: "https://sponsors.vuejs.org/images/bit.svg?v2",
+      },
+      {
+        sponsorUrl: "https://www.vuemastery.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/vuemastery.png",
+      },
+      {
+        sponsorUrl:
+          "https://vueschool.io/?utm_source=Vuejs.org&utm_medium=Banner&utm_campaign=Sponsored%20Banner&utm_content=V1",
+        imgUrl: "https://sponsors.vuejs.org/images/vueschool.png",
+      },
+      {
+        sponsorUrl: "https://vehikl.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/vehikl.png",
+      },
+      {
+        sponsorUrl: "https://passionatepeople.io/",
+        imgUrl: "https://sponsors.vuejs.org/images/passionate_people.png",
+      },
+      {
+        sponsorUrl: "https://www.storyblok.com",
+        imgUrl: "https://sponsors.vuejs.org/images/storyblok.png",
+      },
+      {
+        sponsorUrl:
+          "https://ionicframework.com/vue?utm_source=partner&utm_medium=referral&utm_campaign=vuesponsorship&utm_content=vuedocs",
+        imgUrl: "https://sponsors.vuejs.org/images/ionic.png?v2",
+      },
+      {
+        sponsorUrl: "https://skilled.yashio-corp.com",
+        imgUrl: "https://sponsors.vuejs.org/images/skilled.svg",
+      },
+      {
+        sponsorUrl: "https://opencollective.com/2021-frameworks-fund",
+        imgUrl: "https://sponsors.vuejs.org/images/chrome_frameworks_fund.png",
+      },
+      {
+        sponsorUrl: "https://www.herodevs.com/support/vue",
+        imgUrl: "https://sponsors.vuejs.org/images/herodevs.png",
+      },
+      {
+        sponsorUrl: "https://vuejs.org/sponsor/",
+        imgUrl: "Become a Sponsor",
+      },
+    ],
+  },
+  {
+    sponsorGrade: "Gold",
+    sponsorDetail: [
+      {
+        sponsorUrl: "https://laravel.com",
+        imgUrl: "https://sponsors.vuejs.org/images/laravel.png",
+      },
+      {
+        sponsorUrl: "https://tidelift.com/subscription/npm/vue",
+        imgUrl: "https://sponsors.vuejs.org/images/tidelift.png",
+      },
+      {
+        sponsorUrl: "https://intygrate.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/intygrate.png",
+      },
+      {
+        sponsorUrl: "https://www.y8.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/y8.png",
+      },
+      {
+        sponsorUrl: "https://js.devexpress.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/devexpress.png",
+      },
+      {
+        sponsorUrl: "https://fastcoding.jp/javascript/ ",
+        imgUrl: "https://sponsors.vuejs.org/images/fastcoding_inc.svg",
+      },
+      {
+        sponsorUrl: "https://www.lycorp.co.jp/en/",
+        imgUrl: "https://sponsors.vuejs.org/images/ly_corporation.png?v2",
+      },
+      {
+        sponsorUrl: "https://www.fenetre.nl/",
+        imgUrl:
+          "https://sponsors.vuejs.org/images/fen_tre_online_solutions.svg",
+      },
+      {
+        sponsorUrl: "https://antdv.com",
+        imgUrl: "https://sponsors.vuejs.org/images/ant_design_vue.png",
+      },
+      {
+        sponsorUrl: "https://crisp.chat/en/",
+        imgUrl: "https://sponsors.vuejs.org/images/crisp.png",
+      },
+      {
+        sponsorUrl:
+          "https://localazy.com/blog/how-to-localize-vuejs-app-with-vue-i18n-and-localazy?utm_source=vuejs&utm_medium=banner&utm_campaign=sponsorships_vuejs&utm_content=logo",
+        imgUrl: "https://sponsors.vuejs.org/images/localazy.svg",
+      },
+      {
+        sponsorUrl: "https://casinoburst.com/casino-utan-licens/",
+        imgUrl: "https://sponsors.vuejs.org/images/casinoburst_com.png",
+      },
+      {
+        sponsorUrl: "https://www.enkrypt.com",
+        imgUrl:
+          "https://sponsors.vuejs.org/images/enkrypt__ethereum_and_polkadot_web3_wallet.svg",
+      },
+      {
+        sponsorUrl: "https://www.uudetkasinot.com",
+        imgUrl: "https://sponsors.vuejs.org/images/uudetkasinot_com.png",
+      },
+      {
+        sponsorUrl: "https://usefathom.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/fathom_analytics.svg",
+      },
+      {
+        sponsorUrl: "https://goread.io/buy-instagram-followers",
+        imgUrl: "https://sponsors.vuejs.org/images/goread_io.png",
+      },
+      {
+        sponsorUrl:
+          "https://sentry.io/for/vue?utm_source=vuejs.org&utm_medium=paid-community",
+        imgUrl: "https://sponsors.vuejs.org/images/sentry.png",
+      },
+      {
+        sponsorUrl: "https://poprey.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/poprey_com.png",
+      },
+      {
+        sponsorUrl: "https://www.vedonlyontibonukset.com/pitkavetovihjeet",
+        imgUrl:
+          "https://sponsors.vuejs.org/images/ilmaiset_pitk_vetovihjeet.png",
+      },
+      {
+        sponsorUrl: "https://famoid.com/",
+        imgUrl: "https://sponsors.vuejs.org/images/famoid.png",
+      },
+      {
+        sponsorUrl: "https://www.certible.com",
+        imgUrl: "https://sponsors.vuejs.org/images/certible.svg",
+      },
+      {
+        sponsorUrl: "https://www.fortunegames.com",
+        imgUrl: "https://sponsors.vuejs.org/images/fortune_games.png",
+      },
+      {
+        sponsorUrl: "http://tbdc.com.br/",
+        imgUrl: "https://sponsors.vuejs.org/images/tbdc___agro_software.svg",
+      },
+      {
+        sponsorUrl: "https://fineproxy.org/",
+        imgUrl: "https://sponsors.vuejs.org/images/fineproxy.svg",
+      },
+      {
+        sponsorUrl:
+          "https://rxdb.info/?utm_source=sponsor&utm_medium=opencollective&utm_campaign=opencollective-vuejs",
+        imgUrl: "https://sponsors.vuejs.org/images/daniel.png",
+      },
+      {
+        sponsorUrl: "https://surveyjs.io/",
+        imgUrl: "https://sponsors.vuejs.org/images/surveyjs.png",
+      },
+      {
+        sponsorUrl: "https://vuejs.org/sponsor/",
+        imgUrl: "Become a Sponsor",
+      },
+    ],
+  },
+]);
 </script>
 
 <template>
   <section class="sponsor__container">
-    <h2 class="sponsor__h2">Platinum Sponsor</h2>
-    <div class="sponsor__cards__platinum">
-      <a
-        v-for="(image, imageIndex) in platinumImage"
-        :key="imageIndex"
-        class="sponsor__card"
-      >
-        <img :src="image" alt="Sponsor Image" class="sponsor__card__image" />
-      </a>
-    </div>
-    <h2 class="sponsor__h2">Gold Sponsor</h2>
-    <div class="sponsor__cards__gold">
-      <a
-        v-for="(image, imageIndex) in goldImage"
-        :key="imageIndex"
-        class="sponsor__card"
-      >
-        <img :src="image" alt="Sponsor Image" class="sponsor__card__image" />
-      </a>
-    </div>
+    <h2
+      v-for="(sponsor, sponsorIndex) in imageList"
+      :key="sponsorIndex"
+      class="sponsor__h2"
+    >
+      {{ sponsor.sponsorGrade }} Sponsors
+      <div :class="`sponsor__cards__${sponsor.sponsorGrade.toLowerCase()}`">
+        <a
+          v-for="(image, imageIndex) in sponsor.sponsorDetail"
+          :key="imageIndex"
+          class="sponsor__card"
+          :href="image.sponsorUrl"
+        >
+          <span
+            v-if="image.sponsorUrl === 'https://vuejs.org/sponsor/'"
+            class="sponsor__item"
+            >{{ image.imgUrl }}</span
+          >
+
+          <img
+            v-if="image.sponsorUrl !== 'https://vuejs.org/sponsor/'"
+            :src="image.imgUrl"
+            alt="Sponsor Image"
+            class="sponsor__card__image"
+          />
+        </a>
+      </div>
+    </h2>
   </section>
 </template>
 <style lang="scss">
@@ -56,8 +217,14 @@ console.log("goldImage : ", goldImage.value);
 :after {
   box-sizing: border-box;
 }
+$--sponsor-text-light-3: rgba(60, 60, 60, 0.33);
 
 .sponsor {
+  &__item {
+    font-size: 11px;
+    color: $--sponsor-text-light-3;
+    font-weight: 400;
+  }
   &__container {
     --sponsor-fill-card: #f9f9f9;
     max-width: 900px;
@@ -67,10 +234,11 @@ console.log("goldImage : ", goldImage.value);
   &__h2 {
     font-size: 20px;
     font-weight: 600;
-    margin-bottom: 1em;
+    // margin-bottom: 1em;
   }
   &__cards {
     &__platinum {
+      margin-top: 1rem;
       --max-width: 240px;
       @include large() {
         --max-width: 180px;
@@ -81,6 +249,7 @@ console.log("goldImage : ", goldImage.value);
       column-gap: 4px;
     }
     &__gold {
+      margin-top: 1rem;
       --max-width: 180px;
       @include large() {
         --max-width: 140px;
--- front-vue-ts/src/composables/use-items.ts
+++ front-vue-ts/src/composables/use-items.ts
@@ -1,14 +1,11 @@
 export type SponsorItem = {
-  imgUrl: string;
   sponsorUrl: string;
+  imgUrl: string;
 };
-
-// 231212 정은우: test 이미지에 url을 동적으로 삽입하는 방법을 찾지 못해 일단 이미지만 불러오는 방법으로 해봅니다.
-export type SponsorImage = string[]
 
 export type SponsorItemType = {
   sponsorGrade: string;
-  sponsorDetail: SponsorImage[];
+  sponsorDetail: SponsorItem[];
 };
 
 export type SponsorItemListType = SponsorItemType[]
